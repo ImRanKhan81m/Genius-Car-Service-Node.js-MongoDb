a3accc31c70a1f5090c6fd08e31fccbae2f0fc65
Imran Hossen <freelancerimran81@gmail.com>
2022-04-24 21:20:25 +0600

    custom hook added

--- src/components/Pages/Checkout/Checkout.js
+++ src/components/Pages/Checkout/Checkout.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import useServiceDetail from '../../../hooks/useServiceDetail';
 
 const Checkout = () => {
+    const {serviceId} = useParams();
+    const [service] = useServiceDetail(serviceId);
     return (
         <div style={{height:'81vh'}}>
-            <h1 className='mt-4'>Please Checkout your booking</h1>
+            <h1 className='mt-4'>Please Order: {service.name}</h1>
         </div>
     );
 };
--- src/components/Pages/ServiceDetail/ServiceDetail.js
+++ src/components/Pages/ServiceDetail/ServiceDetail.js
@@ -2,21 +2,17 @@
 import React, { useEffect, useState } from 'react';
 import { Button } from 'react-bootstrap';
 import { Link, useParams } from 'react-router-dom';
+import useServiceDetail from '../../../hooks/useServiceDetail';
 
 const ServiceDetail = () => {
     const { serviceId } = useParams();
-    const [service, setService] = useState({});
-    useEffect(() => {
-        const url = `http://localhost:5000/service/${serviceId}`;
-        console.log(url);
-        fetch(url)
-            .then(res => res.json())
-            .then(data => setService(data))
-    }, [])
+    const [service] = useServiceDetail(serviceId);
+
+    
     return (
         <div className='container py-5' style={{ height: '84vh' }}>
             <h2 className='mb-5'>You are about to book: {service.name}</h2>
-            <Link to="/checkout">
+            <Link to={`/checkout/${serviceId}`}>
                 <Button className='btn btn-primary' >Proceed Checkout</Button>
             </Link>
         </div>
